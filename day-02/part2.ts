f0791f15ef4c9a6ee245686257391be6c788f414
Matt Balmer <signup@mattbalmer.com>
2023-12-02 12:26:48 -0500

    pt2

--- day-02/part2.ts
+++ day-02/part2.ts
@@ -1,5 +1,16 @@
 import { Execute } from './format';
+import { Game, lineToGame, maxSets } from './shared';
+import { mult, sum } from '@utils/array';
+
+const powerOfGame = (game: Game): number => {
+  const maxSeen = maxSets(game.sets);
+  return mult(Object.values(maxSeen));
+}
 
 export const execute: Execute = (lines) => {
-  return 4;
+  const games = lines.map(lineToGame);
+
+  return sum(
+    games.map(powerOfGame),
+  )
 }
