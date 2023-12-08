8a3bf2f46dd590da706c300495d2c2024249a0d8
Matt Balmer <signup@mattbalmer.com>
2023-12-08 12:59:20 -0500

    duh

--- day-08/part2.ts
+++ day-08/part2.ts
@@ -1,10 +1,25 @@
 import { Execute } from './format';
 import { memoize } from '@utils/fn';
+import { mult } from '@utils/array';
 
 type Node = 'string';
 type Connections = [left: Node, right: Node];
 
 const CHAR_INDEX = 'LR';
+
+function lcm(numbers) {
+  function gcd(a, b) {
+    // If the second argument is 0, return the first argument (base case)
+    if (b === 0) {
+      return a;
+    }
+    // Otherwise, recursively call gcd with arguments b and the remainder of a divided by b
+    return gcd(b, a % b);
+  }
+  // Reduce the array of numbers by multiplying each number together and dividing by their gcd
+  // This finds the Least Common Multiple (LCM) of the numbers in the array
+  return numbers.reduce((a, b) => a * b / gcd(a, b));
+}
 
 export const execute: Execute = (lines) => {
   const SEQUENCE = lines[0].split('');
@@ -18,20 +33,20 @@ export const execute: Execute = (lines) => {
     }
   }, {} as Record<Node, Connections>);
 
-  let nodes = Object.keys(map).filter(key => key[2] === 'A');
-  console.log('starting nodes', nodes);
-  let steps = 0;
-  let si = 0;
-  let zCount = 0;
+  const nodes = Object.keys(map).filter(key => key[2] === 'A');
 
-  while (zCount < nodes.length) {
-    const char = SEQUENCE[si++ % SEQUENCE.length];
-    console.log('nodes', si, char, zCount, nodes.join(', '));
-    const i = CHAR_INDEX.indexOf(char);
-    nodes = nodes.map(node => map[node][i]);
-    steps++;
-    zCount = nodes.filter(node => node[2] === 'Z').length;
-  }
+  const factors = nodes.map(node => {
+    let steps = 0;
+    while (node[2] !== 'Z') {
+      SEQUENCE.forEach(char => {
+        const i = CHAR_INDEX.indexOf(char);
+        node = map[node][i];
+        steps++;
+      });
+    }
+    return steps;
+  });
 
-  return steps;
+  console.log(factors);
+  return lcm(factors);
 }
