89d2a025399f032c9d3672e2fe2f394051eafad0
Matt Balmer <signup@mattbalmer.com>
2023-12-08 13:25:22 -0500

    not bad

--- day-08/part1.ts
+++ day-08/part1.ts
@@ -1,6 +1,6 @@
 // 323 chars
 
-export const execute = (l) => {
+export const execute = l => {
   let m = l.slice(2).reduce((m, d) => {
     const [, n, l, r] = d.match(/([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)/);
     m[n] = [l, r];
@@ -10,11 +10,9 @@ export const execute = (l) => {
   let n = 'AAA';
   let s = 0;
 
-  while (n != 'ZZZ') {
-    n = m[n][
-      'LR'.indexOf(l[0][s++ % l[0].length])
-    ];
+  while (n !== 'ZZZ') {
+    n = m[n][['L', 'R'].indexOf(l[0][s++ % l[0].length])];
   }
 
   return s;
-}
+};
--- day-08/part2.ts
+++ day-08/part2.ts
@@ -1,12 +1,9 @@
 // 530 chars
 
-let g = (a, b) =>
-  b === 0 ? a : g(b, a % b);
+let g = (a, b) => (b === 0 ? a : g(b, a % b));
+let lc = n => n.reduce((a, b) => a * b / g(a, b));
 
-let lc = n =>
-  n.reduce((a, b) => a * b / g(a, b));
-
-export const execute = (l) => {
+export const execute = l => {
   let m = l.slice(2).reduce((m, d) => {
     const [, n, l, r] = d.match(/([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)/);
     m[n] = [l, r];
@@ -19,11 +16,9 @@ export const execute = (l) => {
     n.map(n => {
       let s = 0;
       while (n[2] != 'Z') {
-        n = m[n][
-          'LR'.indexOf(l[0][s++ % l[0].length])
-        ];
+        n = m[n]['LR'.indexOf(l[0][s++ % l[0].length])];
       }
       return s;
     })
   );
-}
+};
